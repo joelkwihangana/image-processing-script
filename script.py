94f703d7e16bab569631c47adf8ddfa648d27081
Joel Kwihangana <joelkwijoe@gmail.com>
2025-08-30 21:13:29 +0200

    Loop through input folder and filter only image files

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
 import os
 from PIL import Image
+
+input_folder="images/"
+output_folder="processed/"
+
+# Loop through file names 
+for filename in os.listdir(input_folder):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        file_path=os.path.join(input_folder,filename)
+        print("Processing:",file_path)
